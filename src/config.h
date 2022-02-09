b1464bb764a7664ca0c06979ff5d2b64c067b321
nathantgray <nathantgray@gmail.com>
2022-02-09 09:34:42 -0800

    add pin maps to config.h

--- src/config.h
+++ src/config.h
@@ -1,6 +1,31 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
+
+//pins
+#define SDA 21
+#define SCL 22
+#if WATCHY_HW_VERSION == 15
+#define BATT_ADC_PIN 35
+#else
+#define BATT_ADC_PIN 33
+#endif
+#define RTC_INT_PIN GPIO_NUM_27
+#define DISPLAY_CS 5
+#define DISPLAY_DC 10
+#define DISPLAY_RES 9
+#define DISPLAY_BUSY 19
+#define VIB_MOTOR_PIN 13
+#define MENU_BTN_PIN 26
+#define BACK_BTN_PIN 25
+#define UP_BTN_PIN 32
+#define DOWN_BTN_PIN 4
+#define MENU_BTN_MASK GPIO_SEL_26
+#define BACK_BTN_MASK GPIO_SEL_25
+#define UP_BTN_MASK GPIO_SEL_32
+#define DOWN_BTN_MASK GPIO_SEL_4
+#define ACC_INT_MASK GPIO_SEL_14
+#define BTN_PIN_MASK (MENU_BTN_MASK|BACK_BTN_MASK|UP_BTN_MASK|DOWN_BTN_MASK)
 //display
 #define DISPLAY_WIDTH 200
 #define DISPLAY_HEIGHT 200
